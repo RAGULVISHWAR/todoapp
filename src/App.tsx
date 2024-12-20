94d4943d62c8d29894c5191c45e602eab9b89dde
Ragul <ragulvishwa42@gmail.com>
2024-12-20 11:49:06 +0530

    clearedwarning in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import Createtask from "./components/Createtask"
 import Showtask from "./components/Showtask";
-import { useTaskcontext } from "./context/Taskcontext";
+
 
 
 function App() {
-  const{tasks}=useTaskcontext();
   return (
     <>
      <Createtask />
